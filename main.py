a2d867b486a0bdcbb2604884e55dc588391f0a5a
ME0ny <ipatov.987654321@gmail.com>
2021-10-08 21:22:57 +1000

    some change

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import pygame
 from pygame.constants import CONTROLLER_BUTTON_LEFTSTICK
 from pygame.scrap import put
+pygame.init()
+
+class Window():
+    def __init__(self, width = 500, height = 500, fps = 30, name = "TicTac"):
+        WIN_WIDTH = 500
+        WIN_HEIGHT = 500
+        FPS = 30
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.start_image = pygame.image.load("wallpaper.png").convert()
+        self.field_image = pygame.image.load("field.png").convert()
+        self.tic = pygame.image.load("Tic.png").convert_alpha()
+        self.tac = pygame.image.load("Tac.png").convert_alpha()
+    def pre_render(self):
+        self.screen.fill(WHITE)
+        self.screen.blit(self.start_image, (0, 0))
+    def render_field(self):
+        for i in range(5):
+            pygame.draw.line(self.screen, BLACK, (56 + 97 * i, 55), (56 + 97 * i, 445), 3)
+        for i in range(5):
+            pygame.draw.line(self.screen, BLACK, (55, 56 + 97 * i), (445, 56 + 97 * i), 3)
+        for i in range(4):
+            for j in range(4):
+                if field[i][j] == 1:
+                    self.screen.blit(self.tac, (j * 98  + 58 - j + 12, i * 98  + 58 - i + 10))
+                elif field[i][j] == -1:
+                    self.screen.blit(self.tic, (j * 98  + 58 - j + 15, i * 98  + 58 - i + 14))
 
 def check_win(field):
     for i in range(4):
@@ -44,16 +71,6 @@ def score_count(field):
                     scores[0] += 1
     return (scores)
 
-def init(caption):
-    pygame.init()
-    screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-    pygame.display.set_caption(caption)
-    clock = pygame.time.Clock()
-    return screen, clock
-
-WIN_WIDTH = 500
-WIN_HEIGHT = 500
-FPS = 30
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -62,13 +79,8 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 GRAY = (200, 200, 200)
 
-screen, clock = init('Гонки')
-start_image = pygame.image.load("wallpaper.png").convert()
-field_image = pygame.image.load("field.png").convert()
-tic = pygame.image.load("Tic.png").convert_alpha()
-tac = pygame.image.load("Tac.png").convert_alpha()
 run = True
-player = pygame.Rect(58, 58, 94, 94)
+button = pygame.Rect(189, 467, 122, 23)
 mouse_pos = [0, 0]
 field = [[0, 0, 0, 0] for i in range(4)]
 field_pos = [0, 0]
@@ -76,11 +88,12 @@ click_flag = False
 turn = 1
 turn_count = 0
 basic_font = pygame.font.SysFont(None, 36)
+btn_font = pygame.font.SysFont(None, 12)
 result = 0
+step = 0
+window = Window()
 while run:
-    screen.fill(WHITE)
-    screen.blit(start_image, (0, 0))
-    # обработка событий мыши  клавиатуры:
+    window.pre_render()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -95,13 +108,17 @@ while run:
     if (mouse_pos[0] >= 58 and mouse_pos[0] <= 442 and mouse_pos[1] >= 58 and mouse_pos[1] <= 442 and result == 0):
         field_pos = [(mouse_pos[0] - 58) // 98, (mouse_pos[1] - 58) // 98]
         if (click_flag == True):
+            click_flag = False
             if (field[field_pos[1]][field_pos[0]] == 0):
-                field[field_pos[1]][field_pos[0]] = int(turn)
-                player.x = field_pos[0] * 98  + 58 - field_pos[0]
-                player.y = field_pos[1] * 98  + 58 - field_pos[1]
-                turn /= -1
+                if (step < 2):
+                    turn = 1
+                    field[field_pos[1]][field_pos[0]] = int(turn)
+                elif (step < 4):
+                    turn = -1
+                    field[field_pos[1]][field_pos[0]] = int(turn)
+                step += 1
+                step %= 4
                 turn_count += 1
-
     if (result == 0):
         result = check_win(field)
     if (turn_count == 16 and result == 0):
@@ -122,17 +139,8 @@ while run:
     if (result != 0):
         text = basic_font.render(winner, True, WHITE, BLUE)
         text_pos = (100, 10)
-        screen.blit(text, text_pos)
-    for i in range(5):
-        pygame.draw.line(screen, BLACK, (56 + 97 * i, 55), (56 + 97 * i, 445), 3)
-    for i in range(5):
-        pygame.draw.line(screen, BLACK, (55, 56 + 97 * i), (445, 56 + 97 * i), 3)
-    for i in range(4):
-        for j in range(4):
-            if field[i][j] == 1:
-                screen.blit(tac, (j * 98  + 58 - j + 12, i * 98  + 58 - i + 10))
-            elif field[i][j] == -1:
-                screen.blit(tic, (j * 98  + 58 - j + 15, i * 98  + 58 - i + 14))
+        window.screen.blit(text, text_pos)
+    window.render_field()
     # конец прорисовки окна
     pygame.display.update()
-    clock.tick(10)
+    window.clock.tick(10)
